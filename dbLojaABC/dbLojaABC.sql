bb23244066d5c4f68cc7eeb484e1403fba435254
jhonatanjulio <jhonatanjuliogm@gmail.com>
2023-10-17 17:07:51 -0300

    tela de pesquisar usuarios

--- dbLojaABC/dbLojaABC.sql
+++ dbLojaABC/dbLojaABC.sql
@@ -46,7 +46,9 @@ create table tbUsuarios(
 
 -- insert into tbUsuarios(usuario, senha, cod_func) values ('admin', 'admin', 1);
 
--- select func.nome from tbUsuarios as usu inner join tbFuncionarios as func on usu.cod_func <> func.cod_func;
+-- select cod_func from tbFuncionarios where nome = @nome;
+
+-- select func.nome from tbFuncionarios as func inner join tbUsuarios as usu on usu.cod_func = func.cod_func;
 
 -- select * from tbUsuarios where usuario = 'admin' and senha = 'admin';
 
@@ -56,3 +58,12 @@ create table tbUsuarios(
 
 -- select usu.usuario, usu.senha, func.cod_func from tb_Funcionarios as func inner join tb_Usuarios as usu on func.cod_func = usu.cod_func where func.nome = @nome;
 
+-- INSERT INTO `tbfuncionarios` (`cod_func`, `nome`, `email`, `cpf`, `d_Nasc`, `endereco`, `cep`, `numero`, `bairro`, `estado`, `cidade`) VALUES (1, 'Jhonatan Julio', 'jhonatan@gmail.com', '192.824.982-09', '2023-10-05', 'Rua João Batista Franco', '04842-230', '384', 'Parque Grajaú', 'SP', 'São Paulo'),
+-- (2, 'Rafaela Alves', 'rafaela.alves@senacsp.edu.br', '294.526.186-29', '2023-10-05', 'Rua João Batista Franco', '04842-230', '362', 'Parque Grajaú', 'SP', 'São Paulo'),
+-- (3, 'Vitor Teixeira', 'vitor.teixeira@senacsp.edu.br', '420.124.529-67', '2023-10-05', 'Rua João Batista Franco', '04842-230', '323', 'Parque Grajaú', 'SP', 'São Paulo'),
+-- (4, 'Julio Machado', 'julio.machado@gmail.com', '342.512.509-79', '2023-10-05', 'Rua João Batista Franco', '04842-230', '462', 'Parque Grajaú', 'SP', 'São Paulo');
+
+-- select cod_usu+1 from tbUsuarios order by cod_usu desc;
+
+-- Alterar funcionário
+-- update tbUsuarios set usuario = @usuario, senha = @senha where cod_usu = @cod_usu;
